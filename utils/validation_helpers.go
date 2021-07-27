5203b0a9d8d24247e322aefcb47e81f6864706f5
Tzvika Avni <tavni@redhat.com>
2021-07-27 17:12:45 +0300

    refactor jira issue validaion
    Signed-off-by: Tzvika Avni <tavni@redhat.com>

---
+++ utils/validation_helpers.go
@@ -0,0 +1,51 @@
+package utils
+
+import (
+	"reflect"
+	"strings"
+
+	errors "github.com/zgalor/weberr"
+)
+
+type ValidateRule func() error
+
+func ValidateNilField(field interface{}, name string) ValidateRule {
+	return func() error {
+		if reflect.ValueOf(field).IsNil() {
+			return errors.BadRequest.UserErrorf("Missing field '%s'", name)
+		}
+		return nil
+	}
+}
+
+func ValidateNilObject(field interface{}, name string) ValidateRule {
+	return func() error {
+		if reflect.ValueOf(field).IsNil() {
+			return errors.BadRequest.UserErrorf("'%s' is missing", name)
+		}
+		return nil
+	}
+}
+
+func Validate(rules []ValidateRule) error {
+	for _, rule := range rules {
+		if err := rule(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func ValidateStringParameterNotEmpty(param *string, name string) ValidateRule {
+	return func() error {
+		if param == nil {
+			return errors.BadRequest.UserErrorf("Missing field '%s'", name)
+		}
+		if ValidateNilField(param, name) == nil {
+			if strings.ReplaceAll(*param, " ", "") == "" {
+				return errors.BadRequest.UserErrorf("Field '%s' is empty", name)
+			}
+		}
+		return nil
+	}
+}
