748f652f3ff77f16b063f12039c23cf64d5669c2
Tzvika Avni <tavni@redhat.com>
2021-08-11 15:43:59 +0300

    adding support for jira issue components
    Signed-off-by: Tzvika Avni <tavni@redhat.com>

--- pkg/client/jira/client_test.go
+++ pkg/client/jira/client_test.go
@@ -2,6 +2,9 @@ package jira
 
 import (
 	"fmt"
+	"strings"
+
+	"github.com/andygrunwald/go-jira"
 	// nolint
 	. "github.com/onsi/ginkgo"
 	// nolint
@@ -10,33 +13,35 @@ import (
 
 const (
 	testURL   = "http://example.com/api/"
-	issueType = "Bug"
+	issueType = "Incident"
 	project   = "OHSS"
 	reporter  = "ocm.support"
 	summary   = "\"OCM cluster in error detected: cluster id '%s'\""
+	component = "Red Hat OpenShift Cluster Manager"
+	labels    = "no_qe"
 )
 
 var _ = Describe("Jira issue", func() {
 
-	It("Missing client user", func() {
+	It("Reject missing client user", func() {
 		_, err := NewClient("", "pass", testURL)
 		Expect(err).To(HaveOccurred())
 		Expect(err.Error()).To(ContainSubstring("Field 'jira_user' is empty"))
 	})
 
-	It("Missing client password", func() {
+	It("Reject missing client password", func() {
 		_, err := NewClient("user", "", testURL)
 		Expect(err).To(HaveOccurred())
 		Expect(err.Error()).To(ContainSubstring("Field 'jira_pass' is empty"))
 	})
 
-	It("Missing client url", func() {
+	It("Reject missing client url", func() {
 		_, err := NewClient("user", "pass", "")
 		Expect(err).To(HaveOccurred())
 		Expect(err.Error()).To(ContainSubstring("Field 'jira_url' is empty"))
 	})
 
-	It("Missing summary", func() {
+	It("Reject missing summary", func() {
 		jiraClient, err := NewClient("user", "pass", testURL)
 		Expect(err).NotTo(HaveOccurred())
 
@@ -51,7 +56,7 @@ var _ = Describe("Jira issue", func() {
 		Expect(err.Error()).To(ContainSubstring("Missing field 'summary'"))
 	})
 
-	It("Missing project", func() {
+	It("Reject missing project", func() {
 		jiraClient, err := NewClient("user", "pass", testURL)
 		Expect(err).NotTo(HaveOccurred())
 
@@ -66,7 +71,7 @@ var _ = Describe("Jira issue", func() {
 		Expect(err.Error()).To(ContainSubstring("Missing field 'project'"))
 	})
 
-	It("Missing reporter", func() {
+	It("Reject missing reporter", func() {
 		jiraClient, err := NewClient("user", "pass", testURL)
 		Expect(err).NotTo(HaveOccurred())
 
@@ -81,7 +86,7 @@ var _ = Describe("Jira issue", func() {
 		Expect(err.Error()).To(ContainSubstring("Missing field 'reporter'"))
 	})
 
-	It("Missing issue type", func() {
+	It("Reject missing issue type", func() {
 		jiraClient, err := NewClient("user", "pass", testURL)
 		Expect(err).NotTo(HaveOccurred())
 
@@ -94,6 +99,83 @@ var _ = Describe("Jira issue", func() {
 		err = jiraClient.validateFieldsConfig(fieldsConfigurattion)
 		Expect(err).To(HaveOccurred())
 		Expect(err.Error()).To(ContainSubstring("Missing field 'issue_type'"))
+	})
+
+	It("Accept adding components", func() {
+		jiraClient, err := NewClient("user", "pass", testURL)
+		Expect(err).NotTo(HaveOccurred())
+
+		var fieldsConfigComponents []*string
+		newJiraComponent := string(component)
+		fieldsConfigComponents = append(fieldsConfigComponents, &newJiraComponent)
+
+		fieldsConfigurattion := &FieldsConfiguration{
+			Summary:    GetStringAddress(fmt.Sprintf(summary, "1234")),
+			Project:    GetStringAddress(project),
+			Reporter:   GetStringAddress(reporter),
+			IssueType:  GetStringAddress(issueType),
+			Components: fieldsConfigComponents,
+		}
+		err = jiraClient.validateFieldsConfig(fieldsConfigurattion)
+		Expect(err).ToNot(HaveOccurred())
+
+		newIssue := jira.Issue{
+			Fields: &jira.IssueFields{
+				Summary: *fieldsConfigurattion.Summary,
+				Reporter: &jira.User{
+					Name: *fieldsConfigurattion.Reporter,
+				},
+				Type: jira.IssueType{
+					Name: *fieldsConfigurattion.IssueType,
+				},
+				Project: jira.Project{
+					Key: *fieldsConfigurattion.Project,
+				},
+			},
+		}
+		jiraClient.addIssueFields(newIssue, fieldsConfigurattion)
+		Expect(newIssue.Fields.Components).NotTo(BeEmpty())
+		Expect(newIssue.Fields.Components[0].Name).To(ContainSubstring(component))
+	})
+
+	It("Accept adding labels", func() {
+		jiraClient, err := NewClient("user", "pass", testURL)
+		Expect(err).NotTo(HaveOccurred())
+
+		var fieldsConfigLabels []*string
+		jiraLabels := strings.Split(labels, ",")
+		for _, label := range jiraLabels {
+			str := string(label)
+			fieldsConfigLabels = append(fieldsConfigLabels, &str)
+		}
+
+		fieldsConfigurattion := &FieldsConfiguration{
+			Summary:   GetStringAddress(fmt.Sprintf(summary, "1234")),
+			Project:   GetStringAddress(project),
+			Reporter:  GetStringAddress(reporter),
+			IssueType: GetStringAddress(issueType),
+			Labels:    fieldsConfigLabels,
+		}
+		err = jiraClient.validateFieldsConfig(fieldsConfigurattion)
+		Expect(err).ToNot(HaveOccurred())
+
+		newIssue := jira.Issue{
+			Fields: &jira.IssueFields{
+				Summary: *fieldsConfigurattion.Summary,
+				Reporter: &jira.User{
+					Name: *fieldsConfigurattion.Reporter,
+				},
+				Type: jira.IssueType{
+					Name: *fieldsConfigurattion.IssueType,
+				},
+				Project: jira.Project{
+					Key: *fieldsConfigurattion.Project,
+				},
+			},
+		}
+		jiraClient.addIssueFields(newIssue, fieldsConfigurattion)
+		Expect(newIssue.Fields.Labels).NotTo(BeEmpty())
+		Expect(newIssue.Fields.Labels).To(ConsistOf(labels))
 	})
 })
 
