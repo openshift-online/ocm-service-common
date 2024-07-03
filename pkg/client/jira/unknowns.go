4616d5e4be942a4ac2508de1b58282a5f52301ba
Lucas Ponce <lponce@redhat.com>
2024-07-03 15:45:19 +0200

    OCM-9263 | feat: extend unknown fields

--- pkg/client/jira/unknowns.go
+++ pkg/client/jira/unknowns.go
@@ -1,9 +1,13 @@
 package jira
 
 const ProductsCustomField = "customfield_12319040"
+const ClusterIdField = "customfield_12316349"
+const ClusterOrgField = "customfield_12310160"
 
 var known = map[string]string{
-	"Products": ProductsCustomField,
+	"Products":   ProductsCustomField,
+	"ClusterId":  ClusterIdField,
+	"ClusterOrg": ClusterOrgField,
 }
 
 func getUnknownCustomField(unknown string) string {
